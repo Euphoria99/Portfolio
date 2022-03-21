8fed8f414a89980617804ee18516ee5096fc0d11
Pavan Bhat <363pavanbhat@gmail.com>
2022-03-22 02:16:14 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## My Portfolio
+
+<br/>
+
+# Tech Stack
+
+The website is built using the following tech stack and deployed on [Vercel](https://vercel.com)
+
+- [Next.js](https://nextjs.org) for building React Components.
+- [tailwindcss](https://tailwindcss.com) for styling.
+- [React Rough Notation](https://roughnotation.com) for Hero section highlighting.
+- [Vercel](https://vercel.com) for deployments and CI/CD.
+
+# Usage
+
+- Clone the repository using `git clone `
+- Install all the modules by using `npm i` or `npm install` or `yarn`
+- Run the local development server by using `npm run dev` or `yarn dev`
+- Make the required edits and deploy to YOUR GitHub repo for CI/CD.
+
+# Deployment
+
+Deployment can be done in 3 easy steps.
+
+- Login to [Vercel](https://vercel.com) or signup for an account if you don't have one.
+- Create a `New Project` and select YOUR GitHub repository of the portfolio project.
+- Wait for Vercel to deploy your project to production.
---
+++ constants/data.js
@@ -0,0 +1,79 @@
+const userData = {
+  githubUsername: "Euphoria99",
+  name: "Pavan Bhat",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "363pavanbhat@gmail.com",
+  phone: "",
+  address: "Mangalore, Karnataka, India.",
+  projects: [
+    {
+      title: "Volunteer Status Notifier",
+      link: "https://github.com/Euphoria99/Volunteer-Status-Management",
+      imgUrl: "/VSN.jpeg",
+    },
+    {
+      title: "REST API's",
+      link: "https://github.com/Euphoria99/REST_API",
+      imgUrl: "/restV1.png",
+    },
+    {
+      title: "TodoApp | Angular",
+      link: "https://github.com/Euphoria99/TodoApp",
+      imgUrl: "/todoapp.png",
+    },
+    {
+      title: "QrPix",
+      link: "https://github.com/Euphoria99/QrPix",
+      imgUrl: "/qrpix.png",
+    }
+  ],
+  about: {
+    title:
+      "I'm an aspiring Full Stack Developer who loves building products and web applications with the hope of impacting millions of lives",
+    currentProject: "VSN ",
+    currentProjectUrl: "https://github.com/Euphoria99/Volunteer-Status-Management",
+  },
+  experience: [
+    {
+      title: "Under Graduate ",
+      company: "Sahyadri College of Engineering & Management, Mangalore",
+      year: "2022",
+      companyLink: "https://sahyadri.edu.in/",
+      desc: "Major in Computer Science and Engineering ",
+    },
+    {
+      title: "Internship",
+      company: "TheWebPeople",
+      year: "2021-ongoing",
+      companyLink: "https://thewebpeople.in/",
+      desc: "Developing MEAN stack applications for personal and interprise use.",
+    },
+     {
+      title: "Intermediate Course",
+      company: "Sharada Pre - University College , Mangalore",
+      year: "2017",
+      companyLink: "https://sharadapuc.org/",
+      desc: "Studied PCM with Statistics as an optional subject. Also studied Hindi as second language subject.",
+    },
+    {
+      title: "High School",
+      company: "SVS English Medium School Vidyagiri, Bantwal",
+      year: "2015",
+      companyLink: "https://www.facebook.com/svsvidyagiri",
+      desc: "Completed my Primary, Higher Primary and High School",
+    },
+  ],
+  resumeUrl:
+    "https://docs.google.com/document/d/1Y9ZcB5fDNv9ewIeWDhtHizWo6EIxEu8_6a5mJTh9dho/edit?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/pavanbhat_99/",
+    twitter: "https://twitter.com/iampavan4u",
+    linkedin: "https://www.linkedin.com/in/pavan-bhat-7544781aa/",
+    github: "https://github.com/Euphoria99",
+    facebook: "https://www.facebook.com/pavan.bhat.906/",
+    // discord: ""
+  },
+};
+
+export default userData;
